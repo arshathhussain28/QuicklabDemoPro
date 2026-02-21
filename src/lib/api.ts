2188de248cbe401fb8ac2a41a936550619b0c944
Agash234 <akashakash62901@gmail.com>
2026-02-21 12:14:41 +0530

    change in url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { DemoRequest } from '../context/AppDataContext';
 
-export const API_URL = import.meta.env.VITE_API_URL || `http://${window.location.hostname}:5000/api`;
+export const API_URL = `https://quicklabdemopro.onrender.com/api`;
 
 const api = axios.create({
     baseURL: API_URL,
